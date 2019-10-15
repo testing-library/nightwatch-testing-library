47a3472fda38154a88d31ed58f4c4f62467b5750
Ben Monro <benjamin.monro@walmartlabs.com>
2019-10-15 12:18:30 -0700

    add getByText

---
+++ src/commands/getByText.js
@@ -0,0 +1,29 @@
+module.exports.command = function (text, callback) {
+
+    return new Promise((resolve, reject) => {
+
+        const self = this;
+        // eslint-disable-next-line no-shadow
+        this.execute(function (text) {
+            const elm = window.TestingLibraryDom.getByText(document.body, text);
+            const xpath = window.createXPathFromElement(elm);
+
+            return { xpath, tagName: elm.tagName };
+        }, [text], (result) => {
+            const { value: { xpath, tagName } } = result;
+            resolve({ selector: tagName, value: xpath, locatorStrategy: 'xpath' })
+
+        });
+    });
+
+
+    // return this;
+    // const self = this;
+    // return new Promise((res, rej) => {
+    //     const thing = self.execute(function () {
+    //         res(window.TestingLibraryDom.getByText(document.body, 'getByText'));
+    //     }, (result) => {
+    //         console.log(result)
+    //     });
+    // })
+}
